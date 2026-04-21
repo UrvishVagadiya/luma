0c028ab00c5caa1014b08e8868c7087f3055c3e3
Urvish Vagdiya <urvish@gmail.com>
2026-04-21 14:21:59 +0530

    second

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import { RiGeminiFill } from "react-icons/ri";
-import { MdOutlineArrowOutward } from "react-icons/md";
+import { RiGeminiFill } from 'react-icons/ri';
+import { MdOutlineArrowOutward } from 'react-icons/md';
 const Navbar = () => {
   const d = new Date();
 
-  const time = d.toLocaleTimeString("en-IN", {
-    hour: "2-digit",
-    minute: "2-digit",
+  const time = d.toLocaleTimeString('en-IN', {
+    hour: '2-digit',
+    minute: '2-digit',
     hour12: true,
   });
 
@@ -13,27 +13,27 @@ const Navbar = () => {
     d
       .toTimeString()
       .match(/GMT[+-]\d{2}\d{2}/)?.[0]
-      ?.replace(/(\d{2})(\d{2})$/, "$1:$2") || "";
+      ?.replace(/(\d{2})(\d{2})$/, '$1:$2') || '';
 
   return (
-    <div className="p-4  flex justify-between items-center">
-      <div className="text-gray-400 text-[18px] cursor-pointer ">
-        <RiGeminiFill className="hover:text-black transition-tranform duration-300 " />
+    <div className="flex items-center justify-between p-4">
+      <div className="cursor-pointer text-[18px] text-gray-400">
+        <RiGeminiFill className="transition-tranform duration-300 hover:text-black" />
       </div>
-      <div className="flex gap-4 items-center">
-        <div className="uppercase text-[14px] text-gray-400">
+      <div className="flex items-center gap-4">
+        <div className="text-[14px] text-gray-400 uppercase">
           {time} {tz}
         </div>
-        <div className="flex gap-1 group cursor-pointer hover:text-[#656467]  items-center text-gray-400">
-          <div className="capitalize text-[14px] font-semibold">
-            explore events{" "}
+        <div className="group flex cursor-pointer items-center gap-1 text-gray-400 hover:text-[#656467]">
+          <div className="text-[14px] font-semibold capitalize">
+            explore events{' '}
           </div>
-          <div className="transition-transform duration-300 transform  group-hover:-translate-y-px">
+          <div className="transform transition-transform duration-300 group-hover:-translate-y-px">
             <MdOutlineArrowOutward />
           </div>
         </div>
         <div className="">
-          <button className="flex items-center cursor-pointer trasition-transform duration-300 hover:bg-[#656467] justify-center px-3 py-1 rounded-full text-[14px] bg-[#EEE9EA] hover:text-white text-gray-600 ">
+          <button className="trasition-transform flex cursor-pointer items-center justify-center rounded-full bg-[#EEE9EA] px-3 py-1 text-[14px] text-gray-600 duration-300 hover:bg-[#656467] hover:text-white">
             Sign In
           </button>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Navbar from "./components/Navbar"
-
+import Navbar from './components/Navbar';
 
 const page = () => {
   return (
-    <div className=" min-h-screen bg-linear-to-r from-[#E3EEF4] via-25% via-[#F8F4ED] to-[#F8F0F1]">
-        <Navbar/>
+    <div className="min-h-screen bg-linear-to-r from-[#E3EEF4] via-[#F8F4ED] via-25% to-[#F8F0F1]">
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
