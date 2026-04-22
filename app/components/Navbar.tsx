06d4c1fb87fab7dabc2cc1cb5cb5d725dc7ddc2b
Urvish Vagdiya <urvish@gmail.com>
2026-04-22 09:40:52 +0530

    changes in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,31 +16,30 @@ const Navbar = () => {
       ?.replace(/(\d{2})(\d{2})$/, '$1:$2') || '';
 
   return (
-    <div className=" flex items-center justify-between p-4">
+    <div className="flex items-center justify-between p-4">
       <div className="cursor-pointer text-[18px] text-gray-400">
-        <RiGeminiFill className="transition-transform duration-300 hover:text-foreground" />
+        <RiGeminiFill className="hover:text-foreground transition-transform duration-300" />
       </div>
       <div className="flex items-center gap-4">
         <div className="text-[14px] text-gray-400 uppercase">
           {time} {tz}
         </div>
-<<<<<<< Updated upstream
         <div className="group flex cursor-pointer items-center gap-1 text-gray-400 hover:text-[#656467]">
           <div className="text-[14px] capitalize">
-=======
-        <div className="group flex cursor-pointer items-center gap-1 text-gray-400 hover:text-foreground">
-          <div className="text-[14px] font-semibold capitalize">
->>>>>>> Stashed changes
-            explore events{' '}
+            <div className="group hover:text-foreground flex cursor-pointer items-center gap-1 text-gray-400">
+              <div className="text-[14px] font-semibold capitalize">
+                explore events{' '}
+              </div>
+              <div className="transform transition-transform duration-300 group-hover:-translate-y-px">
+                <MdOutlineArrowOutward />
+              </div>
+            </div>
+            <div className="">
+              <button className="bg-nav-btn-bg text-nav-btn-text hover:bg-nav-btn-hover-bg hover:text-nav-btn-hover-text flex cursor-pointer items-center justify-center rounded-full px-3 py-1 text-[14px] duration-300">
+                Sign In
+              </button>
+            </div>
           </div>
-          <div className="transform transition-transform duration-300 group-hover:-translate-y-px">
-            <MdOutlineArrowOutward />
-          </div>
-        </div>
-        <div className="">
-          <button className="flex cursor-pointer items-center justify-center rounded-full bg-nav-btn-bg px-3 py-1 text-[14px] text-nav-btn-text  duration-300 hover:bg-nav-btn-hover-bg hover:text-nav-btn-hover-text">
-            Sign In
-          </button>
         </div>
       </div>
     </div>
